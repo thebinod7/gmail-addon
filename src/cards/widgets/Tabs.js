6f9b0591a72239485cde7db6596fa97e9cb924cb
Binod Chaudhary <thebinod7@gmail.com>
2023-07-26 20:09:43 +0545

    tabs color update

--- src/cards/widgets/Tabs.js
+++ src/cards/widgets/Tabs.js
@@ -1,20 +1,20 @@
-export default function CardFooter({ activeTab = 'Contact' }) {
+import { MENU_TABS } from '../../constants';
+
+export default function CardFooter({ activeTab = MENU_TABS.CONTACT }) {
 	const contactTabAction = CardService.newAction().setFunctionName('handleContactTabClick').setParameters({});
 
 	let btnContactTab = CardService.newTextButton()
 		.setText('Contact')
-		.setTextButtonStyle(
-			activeTab === 'Contact' ? CardService.TextButtonStyle.FILLED : CardService.TextButtonStyle.TEXT
-		)
+		.setTextButtonStyle(CardService.TextButtonStyle.FILLED)
+		.setBackgroundColor(activeTab === MENU_TABS.CONTACT ? '#fbbc04' : '#F3D07E')
 		.setOnClickAction(contactTabAction);
 
 	const updateTabAction = CardService.newAction().setFunctionName('handleUpdateTabClick').setParameters({});
 
 	let btnUpdatesTab = CardService.newTextButton()
 		.setText('Updates')
-		.setTextButtonStyle(
-			activeTab === 'Updates' ? CardService.TextButtonStyle.FILLED : CardService.TextButtonStyle.TEXT
-		)
+		.setTextButtonStyle(CardService.TextButtonStyle.FILLED)
+		.setBackgroundColor(activeTab === MENU_TABS.UPDATES ? '#fbbc04' : '#F3D07E')
 		.setOnClickAction(updateTabAction);
 
 	return { BtnContactTab: btnContactTab, BtnUpdatesTab: btnUpdatesTab };
